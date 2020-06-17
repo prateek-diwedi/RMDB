788790c8d1bb05e06624caffc3374e50d4fb84f6
Prateek Diwedi <diwedi1112@gmail.com>
2020-06-18 02:20:44 +0530

    paggination added to home page and search page

--- src/components/Landing Page/LandingPage.jsx
+++ src/components/Landing Page/LandingPage.jsx
@@ -7,6 +7,12 @@ function LandingPage() {
 
   const [data, setData] = useState([]);
 
+  const [page, setPage] = useState(1)
+
+  const [totalPages, setTotalPages] = useState(null)
+
+  console.log('page in searchpoage ---->', page)
+  console.log('totla in searchpoage ---->', totalPages)
 
   // link added infront of the image data we are getting from api
   const imagelink = 'https://image.tmdb.org/t/p/w440_and_h660_face/'
@@ -15,24 +21,47 @@ function LandingPage() {
   const defaultImage = 'https://images.all-free-download.com/images/graphiclarge/creative_cinema_art_backgrounds_vectors_581202.jpg';
 
   // popular movies url
-  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_TMDB_API_KEY}`
+  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_TMDB_API_KEY}&page=${page}`
 
   // fetching data for popular movies
   useEffect(() => {
     axios.get(url)
       .then((res) => {
-        // console.log(res.data.results[0].id)
-        const apiData = res.data.results
-        setData(apiData);
+        console.log(res.data)
+        const apiData = res.data
+        setData(apiData.results);
+        setTotalPages(apiData.total_pages)
       }, (err) => {
         console.log('Error while fetching data ---->', err)
       })
-  }, [])
+  }, [page])
 
   const movieCardData = data.map((moviesData, index) => {
-    return <MovieCard key={moviesData.id} title={moviesData.title || moviesData.name} rating={moviesData.vote_average * 10} poster={
-      `${imagelink}${moviesData.poster_path}` || defaultImage} />
+    var imagePath;
+    if (moviesData.poster_path == null || moviesData.poster_path == "") {
+      imagePath = defaultImage;
+    } else {
+      imagePath = imagelink + moviesData.poster_path;
+    }
+    
+    return <MovieCard key={moviesData.id || Math.random() * 100} title={moviesData.title || moviesData.name} rating={moviesData.vote_average * 10 || 0} poster={imagePath} />
   })
+
+  //next page funbction for pagination
+  const nextPage = () => {
+    console.log('next page')
+    if (data && page !== totalPages) {
+      setPage(page + 1)
+    }
+  }
+
+  //  previous page function for pagination
+  const previousPage = () => {
+    console.log('previous page')
+    if (data && page !== 1) {
+      setPage(page - 1)
+    }
+  }
 
   return (
     <div>
@@ -50,6 +79,8 @@ function LandingPage() {
             <div className="card-columns" style={{ padding: '5%', marginLeft: '9%' }} >
               {movieCardData}
             </div>
+            <button onClick={previousPage} > Previous </button>
+            <button onClick={nextPage} > Next </button>
           </div>
         )}
 
--- src/components/searchPage/SearchPage.jsx
+++ src/components/searchPage/SearchPage.jsx
@@ -5,6 +5,12 @@ import SearchBox from '../GeneralComponent/SearchBox'
 
 function SearchPage(props) {
   const [data, setData] = useState([]);
+
+  const [page, setPage] = useState(1)
+
+  const [totalPages, setTotalPages] = useState(null)
+
+  console.log('page in searchpoage ---->', page)
   console.log('props in search page', props.match.params.movie)
   // link added infront of the image data we are getting from api
   const imagelink = 'https://image.tmdb.org/t/p/w440_and_h660_face/'
@@ -13,31 +19,48 @@ function SearchPage(props) {
   const defaultImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTWAFqXrJQ-ZGNDEA0AVJXXF2p55MWpuHRujST0xH9qFjiFNpln&usqp=CAU';
 
   // popular movies url
-  const url = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=${props.match.params.movie}&page=1&include_adult=false`
+  const url = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=${props.match.params.movie}&page=${page}&include_adult=false`
 
   // fetching data for popular movies
   useEffect(() => {
     axios.get(url)
       .then((res) => {
         console.log(res);
-        console.log(res.data.results);
-        const apiData = res.data.results;
-        setData(apiData);
+        console.log(res.data);
+        const apiData = res.data;
+        setData(apiData.results);
+        setTotalPages(apiData.total_pages)
       }, (err) => {
         console.log('Error while fetching data ---->', err)
       })
-  }, [])
+  }, [page])
 
   const searchedMovieData = data.map((moviesData, index) => {
     var imagePath;
-    if(moviesData.poster_path == null || moviesData.poster_path == ""){
+    if (moviesData.poster_path == null || moviesData.poster_path == "") {
       imagePath = defaultImage;
-    }else{
-      imagePath = imagelink+moviesData.poster_path;
+    } else {
+      imagePath = imagelink + moviesData.poster_path;
     }
 
-    return <MovieCard key={moviesData.id||Math.random()*100} title={moviesData.title || moviesData.name} rating={moviesData.vote_average * 10||0} poster={imagePath} />
+    return <MovieCard key={moviesData.id || Math.random() * 100} title={moviesData.title || moviesData.name} rating={moviesData.vote_average * 10 || 0} poster={imagePath} />
   })
+
+  //next page funbction for pagination
+  const nextPage = () => {
+    console.log('next page')
+    if (data && page !== totalPages) {
+      setPage(page + 1)
+    }
+  }
+
+  //  previous page function for pagination
+  const previousPage = () => {
+    console.log('previous page')
+    if (data && page !== 1) {
+      setPage(page - 1)
+    }
+  }
 
   return (
     <div>
@@ -47,12 +70,15 @@ function SearchPage(props) {
 
       <SearchBox />
 
-     
-            <h2 style={{ paddingLeft: '14%' }}> Showing Results For {props.match.params.movie} </h2>
-            <div className="card-columns" style={{ padding: '5%', marginLeft: '9%' }} >
-              {searchedMovieData}
-            </div>
-        
+
+      <h2 style={{ paddingLeft: '14%' }}> Showing Results For {props.match.params.movie} </h2>
+      <div className="card-columns" style={{ padding: '5%', marginLeft: '9%' }} >
+        {searchedMovieData}
+      </div>
+
+      <button onClick={previousPage} > Previous </button>
+      <button onClick={nextPage} > Next </button>
+
     </div>
   )
 }

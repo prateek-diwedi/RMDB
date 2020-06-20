0220ac802c3a0320ee51d530abf611859461aa46
Prateek Diwedi <diwedi1112@gmail.com>
2020-06-20 17:34:27 +0530

    added logo

--- src/components/Landing Page/LandingPage.jsx
+++ src/components/Landing Page/LandingPage.jsx
@@ -3,6 +3,7 @@ import MovieCard from '../GeneralComponent/MovieCard'
 import axios from 'axios'
 import SearchBox from '../GeneralComponent/SearchBox'
 import '../GeneralComponent/button.scss'
+import myLogo from '../../images/logo.png'
 
 
 function LandingPage() {
@@ -14,9 +15,6 @@ function LandingPage() {
   const [totalPages, setTotalPages] = useState(null)
 
   const logo = 'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg';
-
-  const myLogo = 'https://github.com/prateek-diwedi/movie-database/blob/master/public/7G0NVl.png?raw=true'
-
 
   // link added infront of the image data we are getting from api
   const imagelink = 'https://image.tmdb.org/t/p/w440_and_h660_face/'

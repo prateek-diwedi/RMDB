6ab34b3799afb7b6080eba483f20ce30817413a0
Prateek Diwedi <diwedi1112@gmail.com>
2020-06-20 18:26:45 +0530

    Updates

---
+++ precache-manifest.1caa4ea412c56db0599ccfe3560acdff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7646defbb70aa7c360b014288959ddbe",
+    "url": "/RMDB/index.html"
+  },
+  {
+    "revision": "3fc7e83ce474e78d3a48",
+    "url": "/RMDB/static/css/2.1ba26301.chunk.css"
+  },
+  {
+    "revision": "ac235b7869137f6a95ca",
+    "url": "/RMDB/static/css/main.8b15051f.chunk.css"
+  },
+  {
+    "revision": "3fc7e83ce474e78d3a48",
+    "url": "/RMDB/static/js/2.601f2311.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RMDB/static/js/2.601f2311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac235b7869137f6a95ca",
+    "url": "/RMDB/static/js/main.9165c1dc.chunk.js"
+  },
+  {
+    "revision": "128f610d6a73fa228ebc",
+    "url": "/RMDB/static/js/runtime-main.55c4b42b.js"
+  },
+  {
+    "revision": "ff8991fecce40bb5767cca02c110df08",
+    "url": "/RMDB/static/media/logo.ff8991fe.png"
+  }
+]);
--- src/components/Movie Page/helper.js
+++
@@ -1,6 +0,0 @@
-export const year = (date) => {
-  if (!date) {
-    return 'N/A'
-  }
-  return date.slice(0, 4)
-}
